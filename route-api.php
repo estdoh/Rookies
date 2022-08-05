c9a1038458864b2fdf64533a19dc1c5d4d631abe
Gaston-doh <gmontaruli@alumnos.exa.unicen.edu.ar>
2022-08-05 15:26:37 -0300

    Ajustes llamadas Api

--- route-api.php
+++ route-api.php
@@ -1,9 +1,7 @@
 <?php
 require_once 'libs/Router.php';
 require_once 'app/ApiProductsController.php';
-require_once 'app/ApiCategoryController.php';
 require_once 'app/ApiUserController.php';
-require_once 'app/ApiCommentsController.php';
 
 // crea el router
 $router = new Router();
@@ -15,8 +13,8 @@ $router->addRoute('products/:category', 'GET', 'ApiProductController', 'getProdu
 $router->addRoute('products/:ID', 'DELETE', 'ApiProductController', 'deleteProduct');
 $router->addRoute('products', 'POST', 'ApiProductController', 'insertProduct');
 $router->addRoute('products/:ID', 'PUT', 'ApiProductController', 'editProduct');
-$router->addRoute('categories', 'GET', 'ApiProductController', 'getCategoriesFromProducts');
 
+$router->addRoute('categories', 'GET', 'ApiProductController', 'getCategoriesFromProducts');
 // $router->addRoute('categories/:ID', 'GET', 'ApiCategoryController', 'getCategory');
 // $router->addRoute('categories/:ID', 'DELETE', 'ApiCategoryController', 'deleteCategory');
 // $router->addRoute('categories', 'POST', 'ApiCategoryController', 'insertCategory');
